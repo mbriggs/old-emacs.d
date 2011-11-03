9179a51eb103a25e5f95e0dcae1fb183123b13ae
Matt Briggs <matt@mattbriggs.net>
2011-11-02 21:44:49 -0400

    added mustache bindings

--- config/mode-bindings.el
+++ config/mode-bindings.el
@@ -2,6 +2,15 @@
 (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
 (add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode))
 
+(autoload 'markdown-mode "markdown-mode" nil t)
+(autoload 'gfm-mode "markdown-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.mkd$" . gfm-mode))
+(add-to-list 'auto-mode-alist '("\\.md$" . gfm-mode))
+(add-to-list 'auto-mode-alist '("\\.text$" . gfm-mode))
+
+(add-to-list 'auto-mode-alist '("\\.jst$" . mustache-mode))
+
 (autoload 'ruby-mode "ruby-mode" nil t)
 (add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Capfile" . ruby-mode))
