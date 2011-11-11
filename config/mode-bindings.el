ea3eb49bb4531e50aaccbae5b9d5b70fe7f2e311
Matt Briggs <matt@mattbriggs.net>
2011-11-10 23:08:54 -0500

    adds more varied support for sass, hbs, and scss

--- config/mode-bindings.el
+++ config/mode-bindings.el
@@ -2,6 +2,13 @@
 (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
 (add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode))
 
+(add-to-list 'auto-mode-alist '("\\.hbs$" . moustache-mode))
+(add-to-list 'auto-mode-alist '("\\.hbs.erb$" . moustache-mode))
+(add-to-list 'auto-mode-alist '("\\.jst$" . mustache-mode))
+
+(add-to-list 'auto-mode-alist '("\\.sass.erb$" . sass-mode))
+(add-to-list 'auto-mode-alist '("\\.scss.erb$" . scss-mode))
+
 (autoload 'markdown-mode "markdown-mode" nil t)
 (autoload 'gfm-mode "markdown-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
@@ -9,7 +16,6 @@
 (add-to-list 'auto-mode-alist '("\\.md$" . gfm-mode))
 (add-to-list 'auto-mode-alist '("\\.text$" . gfm-mode))
 
-(add-to-list 'auto-mode-alist '("\\.jst$" . mustache-mode))
 
 (autoload 'ruby-mode "ruby-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
@@ -24,3 +30,4 @@
 
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.js.erb$" . js2-mode))
