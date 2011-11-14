ab6c91be4336939d612d162e48d1215dd5e56f75
Matt Briggs <matt@mattbriggs.net>
2011-11-14 13:43:53 -0500

    break out the customize-variable stuff into its own file

---
+++ config/customize-variables.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(js2-always-indent-assigned-expr-in-decls-p t)
+ '(js2-auto-indent-p t)
+ '(js2-basic-offset 2)
+ '(js2-consistent-level-indent-inner-bracket-p t)
+ '(js2-enter-indents-newline t)
+ '(js2-highlight-level 3)
+ '(js2-indent-on-enter-key t)
+ '(js2-mirror-mode nil)
+ '(js2-strict-missing-semi-warning nil))
