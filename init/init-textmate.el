034c62eb843efe4b6e054176cf8641e5e5c3ec4b
Matt Briggs <matt@mattbriggs.net>
2011-12-06 10:12:07 -0500

    adds comment-or-uncomment-region-or-line

--- init/init-textmate.el
+++ init/init-textmate.el
@@ -67,4 +67,21 @@ A place is considered `tab-width' character columns."
   (interactive)
   (textmate-shift-right (* -1 (or arg 1))))
 
+(defmacro allow-line-as-region-for-function (orig-function)
+`(defun ,(intern (concat (symbol-name orig-function) "-or-line"))
+   ()
+   ,(format "Like `%s', but acts on the current line if mark is not active."
+            orig-function)
+   (interactive)
+   (if mark-active
+       (call-interactively (function ,orig-function))
+     (save-excursion
+       ;; define a region (temporarily) -- so any C-u prefixes etc. are preserved.
+       (beginning-of-line)
+       (set-mark (point))
+       (end-of-line)
+       (call-interactively (function ,orig-function))))))
+
+(allow-line-as-region-for-function comment-or-uncomment-region)
+
 (provide 'init-textmate)
