345287e15f099ffd807929e489cec2ba58d04499
Matt Briggs <matt@mattbriggs.net>
2011-12-05 23:53:53 -0500

    makes ruby-end also insert after a ret

--- init/init-ruby-end.el
+++ init/init-ruby-end.el
@@ -1,4 +1,16 @@
 (require 'ruby-end)
 (setq ruby-end-insert-newline nil)
 
+(add-hook 'ruby-mode-hook (lambda ()
+                            (ruby-end-mode)
+                            (newline)))
+
+(define-key ruby-end-mode-map (kbd "RET") (lambda ()
+                                            (interactive)
+                                            (if (ruby-end-expand-p)
+                                                (ruby-end-insert-end))
+                                            (let ((ruby-end-mode nil))
+                                              (call-interactively
+                                               (key-binding
+                                                (read-kbd-macro "RET"))))))
 (provide 'init-ruby-end)
