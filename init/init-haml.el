9e7661f5e2e75bfe4bab81bb33ae2487403c44f8
Matt Briggs <matt@mattbriggs.net>
2012-01-24 10:02:27 -0500

    made haml amode more specific to counter nxhtmls pwnage

--- init/init-haml.el
+++ init/init-haml.el
@@ -1,4 +1,6 @@
 (add-to-list 'auto-mode-alist '("\\.haml$" . haml-mode))
+(add-to-list 'auto-mode-alist '("\\.html\\.haml$" . haml-mode))
+
 (add-hook 'haml-mode-hook 'flymake-haml-load)
 (add-hook 'sass-mode-hook 'flymake-sass-load)
 
