7d89e49efe9f81d4d09caa4c3dcf8111715830ba
Matt Briggs <matt@mattbriggs.net>
2012-03-01 15:29:05 -0500

    bug

--- init/init-nxml.el
+++ init/init-nxml.el
@@ -1,6 +1,5 @@
-(require 'nxml)
 
-(define-key nxml-mode-map "\M-}" nil)
-(define-key nxml-mode-map "\M-{" nil)
+;(define-key nxml-mode-map "\M-}" nil)
+;(define-key nxml-mode-map "\M-{" nil)
 
 (provide 'init-nxml)
