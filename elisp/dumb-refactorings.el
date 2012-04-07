71a34d382a885761dbe2cb688d6279ad2192129a
Matt Briggs <matt@mattbriggs.net>
2012-04-07 01:36:16 -0400

    stuff

--- elisp/dumb-refactorings.el
+++ elisp/dumb-refactorings.el
@@ -16,13 +16,17 @@
 ;; Some automated refactorings require a full AST tree of your entire project. Others can be
 ;; implemented in ways that are way more dumb, but still provide value.
 
+;; bind the following functions to keys, or just call them via m-x
+
 ;; - dr/extract-variable   :: give the current region a name, create the variable right above it
 ;;                            and replace the region with the var name
 ;; - dr/inline-variable    :: take the current word the point is on, find what is on the other side
 ;;                            of the equals, delete the line, find the next instance of the variable,
 ;;                            and replace it with its definition
 ;; - dr/rename-in-project  :: do a regexp replace in multiple files for the given project
-;;                            loosely based on mutli-replace by Bernard Vauquelin
+
+
+;;;; extract-variable
 
 (defun dr/extract-variable ()
   "Create variable from region"
@@ -41,6 +45,9 @@
 
 ;; foo = DoSomething(1 + 1)
 
+
+;;;; inline variable
+
 (defun dr/inline-variable ()
   "Remove variable declaration at point, and replace its
    first usage with its definition"
@@ -56,55 +63,69 @@
 ;; blah.blah(foo)
 
 
-;;; rename in proj
+;;;; rename in proj
 
 (defun dr/rename-in-project ()
   "Do the same query-replace on many files."
   (interactive)
-  (let* ((default-search (current-word))
-         (search-input (read-from-minibuffer (concat "Rename (" default-search "): ")))
-         (search (if (string-equal "" search-input) default-search search-input))
+  (let* ((search (dr:rename-string))
          (replace (read-from-minibuffer (concat "Rename '" search "' to: ")))
          (search-root (ido-read-directory-name (concat "Rename '" search "' to '" replace "' in: ")
                                                (eproject-root)))
-         (search-command (concat "fgrep -rl '" search "' " search-root))
          (oldbuf (current-buffer))
-         (outbuf (get-buffer-create "*rename-in-proj-temp*")))
+         (outbuf (dr:create-rename-in-project-buffer))
+         (matched-files (dr:search-for-matches-in-project outbuf)))
 
-    (dr:create-rename-in-project-buffer outbuf)
-    (when (dr:search-for-matches-in-project outbuf search-command)
-      (beginning-of-buffer)
-      (newline)
+    (while matched-files
+      (dr:report-files-left-to-go search-root matched-files)
+      (dr:query-replace-next-file (pop matched-files) search replace))
+    (dr:clean-up oldbuf)))
 
-      (while (> (buffer-size) 1)
-        (dr:report-files-left-to-go)
-        (dr:query-replace-next-file name search replace)))
-    (popwin:close-popup-window)
-    (message "Done.")
-    (switch-to-buffer oldbuf)))
+;; get inputs
 
-(defun dr:search-for-matches-in-project (outbuf search-command)
-  (zerop (call-process "/bin/sh" nil outbuf nil "-c" search-command)))
+(defun dr:rename-string ()
+  (let* ((default-search (current-word))
+         (search-input (read-from-minibuffer (concat "Rename (" default-search "): "))))
+    (if (string-equal "" search-input) default-search search-input)))
 
-(defun dr:create-rename-in-project-buffer (outbuf)
-  (popwin:popup-buffer outbuf :noselect t)
-  (set-buffer outbuf)
-  (erase-buffer))
+(defun dr:create-rename-in-project-buffer ()
+  (let ((outbuf (get-buffer-create "*rename-in-proj*")))
+    (popwin:popup-buffer outbuf :noselect t)
+    (set-buffer outbuf)
+    (erase-buffer)
 
-(defun dr:report-files-left-to-go ()
+    outbuf))
+
+(defun dr:search-for-matches-in-project (outbuf)
+  (zerop (call-process "/bin/sh" nil outbuf nil "-c"
+                       (concat "fgrep -rl '" search "' " search-root)))
+  (split-string (buffer-string) "\n" t))
+
+;; perform rename
+
+(defun dr:report-files-left-to-go (search-root matched-files)
   (beginning-of-buffer)
   (kill-line)
-  (insert (concat (number-to-string (count-lines 2 (point-max))) " <------- files to go"))
+  (let ((current-file (replace-regexp-in-string search-root "" (car matched-files)))
+        (files-left (number-to-string (- (length matched-files) 1))))
+   (insert (concat "Currently On: " current-file ", " files-left " files to go")))
   (next-line)
   (beginning-of-line))
 
-(defun dr:query-replace-next-file (name search replace)
-  (let ((name (buffer-substring (point) (progn (end-of-line) (point)))))
-    (save-excursion
-      (find-file name)
-      (beginning-of-buffer)
-      (query-replace search replace))
-    (beginning-of-line)
-        (kill-line 1)))
+
+(defun dr:query-replace-next-file (file search replace)
+  (save-excursion
+    (find-file file)
+    (beginning-of-buffer)
+    (query-replace search replace))
+  (beginning-of-line)
+  (kill-line 1))
+
+;; misc
+
+(defun dr:clean-up (oldbuf)
+  (popwin:close-popup-window)
+  (message "Done.")
+  (switch-to-buffer oldbuf))
 
 (provide 'dumb-refactorings)
