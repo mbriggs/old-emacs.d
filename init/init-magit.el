3f5fa5c99ad515b04468d483a4e020bd2fa88cea
Matt Briggs <matt@mattbriggs.net>
2012-04-18 23:16:37 -0400

    clear out commit buffer

--- init/init-magit.el
+++ init/init-magit.el
@@ -1,10 +1,12 @@
 (add-hook 'magit-log-edit-mode-hook 'insert-ticket-number-from-branch-name)
 
 (defun insert-ticket-number-from-branch-name ()
+  (erase-buffer)
+  (evil-insert-state)
   (let* ((current-branch (car (vc-git-branches)))
          (ticket-number (replace-regexp-in-string "[0-9]+\\(_.*\\)$" ""
                                                   current-branch nil nil 1)))
-    (if (string-match "^[0-9]" current-branch)
-        (insert (concat "#" ticket-number ": ")))))
+    (when (string-match "^[0-9]" current-branch)
+      (insert (concat "#" ticket-number ": ")))))
 
 (provide 'init-magit)
