9d664377167187a125d0a7b3b890362005a4527f
Matt Briggs <matt@mattbriggs.net>
2012-05-10 20:54:40 -0400

    stuff

--- init/init-ido.el
+++ init/init-ido.el
@@ -10,7 +10,7 @@
 (setq ido-default-buffer-method 'selected-window)
 
 ;; Display ido results vertically, rather than horizontally
-(setq ido-decorations (quote ("\n   " "" "\n   " "\n   ..." "[" "]" " [No match]" " [Matched]" " [Not readable]" " [Too big]" " [Confirm]")))
+(setq ido-decorations '("\n   " "" "\n   " "\n   ..." "{" "}" " [No match]" " [Matched]" " [Not readable]" " [Too big]" " [Confirm]"))
 (defun ido-disable-line-trucation () (set (make-local-variable 'truncate-lines) nil))
 (add-hook 'ido-minibuffer-setup-hook 'ido-disable-line-trucation)
 
