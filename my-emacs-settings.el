aa6f96b227551cee86e484ee36d60d548e1b1ed9
Matt Briggs <matt@mattbriggs.net>
2012-05-14 16:03:33 -0400

    modify some tm.el stuff

--- my-emacs-settings.el
+++ my-emacs-settings.el
@@ -29,6 +29,8 @@
 (set-fringe-style -1)
 (tooltip-mode -1)
 
+(setq textmate-find-files-command "git ls-tree --full-tree --name-only -r HEAD")
+(setq *textmate-gf-exclude* "(/|^)(\\.+[^/]+|vendor|tmp|log|classes|build|public|db/migrate)($|/)|(\\.xcodeproj|\\.nib|\\.framework|\\.app|\\.pbproj|\\.pbxproj|\\.xcode|\\.xcodeproj|\\.bundle|\\.pyc)(/|$)")
 
 ;; only turn off menus if not osx
 (if (not (eq system-type 'darwin))
