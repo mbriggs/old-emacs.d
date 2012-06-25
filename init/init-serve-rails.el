d66e20fffae887efc05ad8ca41ad6544a5f257e3
Matt Briggs <matt@mattbriggs.net>
2012-06-25 12:38:46 -0400

    add more env vars for packman

--- init/init-serve-rails.el
+++ init/init-serve-rails.el
@@ -2,10 +2,12 @@
 (add-hook 'serve-rails/starting-guard-hook 'set-guard-env)
 
 (defun set-spork-env ()
-  (setenv "SPORK_RUNNING" "true"))
+  (setenv "SPORK_RUNNING" "true")
+  (setenv "NO_HYDRA" "true"))
 
 (defun set-guard-env ()
   (setenv "GUARD_JASMINE" "true")
-  (setenv "GUARD_MINITEST" "true"))
+  (setenv "GUARD_MINITEST" "true")
+  (setenv "NO_HYDRA" "true"))
 
 (provide 'init-serve-rails)
