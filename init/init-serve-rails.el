d6790a0e8455766024ea9c730dbfe05eb0e3488b
Matt Briggs <matt@mattbriggs.net>
2012-06-28 02:23:02 -0400

    serve-rails vars

--- init/init-serve-rails.el
+++ init/init-serve-rails.el
@@ -8,6 +8,14 @@
 (defun set-guard-env ()
   (setenv "GUARD_JASMINE" "true")
   (setenv "GUARD_MINITEST" "true")
-  (setenv "NO_HYDRA" "true"))
+  (setenv "GUARD_LIVERELOAD" "true"))
+
+(defun clear-test-env-vars ()
+  (interactive)
+  (setenv "SPORK_RUNNING" "false")
+  (setenv "NO_HYDRA" "false")
+  (setenv "GUARD_JASMINE" "false")
+  (setenv "GUARD_MINITEST" "false")
+  (setenv "GUARD_LIVERELOAD" "false"))
 
 (provide 'init-serve-rails)
