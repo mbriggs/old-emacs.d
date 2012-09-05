424b96c14fe3989748669372a77859399659d4e1
Matt Briggs <matt@mattbriggs.net>
2012-09-05 09:04:10 -0400

    adds marionette to default channels

--- init/init-erc.el
+++ init/init-erc.el
@@ -24,6 +24,7 @@
                                        "#clojure"
                                        "#ruby-talk"
                                        "#emacs"
+                                       "#marionette"
                                        "#nulogy"
                                        "#javascript")))
 
