f2c8d8ae8c3f3694461cea00b5c208f35b568d33
Matt Briggs <matt@mattbriggs.net>
2013-08-29 01:23:46 -0400

    hack magit to not pop open a new window

--- init/init-magit.el
+++ init/init-magit.el
@@ -1,5 +1,6 @@
 (require 'magit)
 
+;(setq magit-status-buffer-switch-function 'switch-to-buffer)
 (setq magit-default-tracking-name-function 'magit-default-tracking-name-branch-only)
 (add-hook 'magit-log-edit-mode-hook 'insert-ticket-number-from-branch-name)
 
@@ -11,5 +12,9 @@
                                                   current-branch nil nil 1)))
     (when (string-match "^[0-9]" current-branch)
       (insert (concat "#" ticket-number ": ")))))
+
+; (defadvice magit-pop-to-log-edit (around magit-prevent-window-popup activate) (let ((pop-up-windows)) ad-do-it)) 
+;(setq display-buffer-function 'display-buffer-same-window)
+(defun magit-use-emacsclient-p ())
 
 (provide 'init-magit)
